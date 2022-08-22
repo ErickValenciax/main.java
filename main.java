f848e081dc3825a67a785042564a479104c3890d
Erick Valencia <72419788+ErickValenciax@users.noreply.github.com>
2022-08-22 15:45:16 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main ( String[] args) {
+		int valor = 0;
+
+		Scanner stdin = new Scanner ( System.in );
+
+		valor = stdin.nextInt();
+		stdin.close();
+    }
+}
